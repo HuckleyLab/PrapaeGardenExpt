6bd48912b5dd19432c97ac6ce968ad91d458da7f
Lauren Buckley <lbbuckley@gmail.com>
2025-02-12 21:24:06 -0800

    update temperature plots

--- TempData.R
+++ TempData.R
@@ -16,7 +16,7 @@ library(readxl)
 #Taylor version: https://github.com/taylorhatcher/WARP2024/tree/main
 
 #toggle between desktop (y) and laptop (n)
-desktop<- "y"
+desktop<- "n"
 
 if(desktop=="y") setwd("/Users/laurenbuckley/Google Drive/Shared drives/TrEnCh/Projects/WARP/Projects/PrapaeGardenExpt/data/WeatherData/")
 if(desktop=="n") setwd("/Users/lbuckley/Google Drive/Shared drives/TrEnCh/Projects/WARP/Projects/PrapaeGardenExpt/data/WeatherData/")
@@ -29,206 +29,120 @@ temp2.1997$Period<- 2
 temp.1997= rbind(temp.1997,temp2.1997)
 #format time
 min <- str_sub(temp.1997$Time,-2,-1)
-min[min %in% c(":0",":5")]<- paste(":0", str_sub(min,-1,-1), sep="")
+min[min %in% c("0","5")]<- paste("0", str_sub(min[min %in% c("0","5")],-1,-1), sep="")
 hr<- str_sub(temp.1997$Time,-4,-3)
 hr[hr==""]<- "0"
 temp.1997$hm<- paste(hr, min, sep=":")
+temp.1997$hr<- as.numeric(hr); temp.1997$min<- as.numeric(min)
+temp.1997$dt<- temp.1997$Date + temp.1997$hr/24 + temp.1997$min/(24*60)
 
+#1999
 temp.1999= read.csv("FormattedHistoricMetDataFieldSln.csv") #8/18-8/25
+temp.1999$hr<- as.numeric(str_sub(temp.1999$LONGTIME,-5,-4))
+temp.1999$dt<- temp.1999$JDATE + as.numeric(str_sub(temp.1999$LONGTIME,-5,-4))/24 +as.numeric(str_sub(temp.1999$LONGTIME,-2,-1))/(24*60)
 
+#2024
 temp.2024= read.csv("combined_loggers_2024.csv") #6/21 - 8/18
+#format time  
+jday<- as.numeric(format(as.Date(temp.2024$datetime, format = "%Y-%m-%d %H:%M:%S"), "%j"))
+temp.2024$hr<- as.numeric(format(strptime(temp.2024$datetime, format = "%Y-%m-%d %H:%M:%S"), "%H"))
+min<- as.numeric(format(strptime(temp.2024$datetime, format = "%Y-%m-%d %H:%M:%S"), "%M"))
+temp.2024$dt= jday +temp.2024$hr/24 + min/(24*60)
+temp.2024$Date<- format(strptime(temp.2024$datetime, format = "%Y-%m-%d %H:%M:%S"), "%Y-%m-%d")
+temp.2024$Time<- format(strptime(temp.2024$datetime, format = "%Y-%m-%d %H:%M:%S"), "%H:%M")
 
-#format data
+#change to long format and combine
+t1997.l <- melt(temp.1997[,c("dt","Date","Time","hr","Tm1","Tm2","Tm3","Tm4","Tm5","Tm6","Tm7","Tm8","Tm9","Tm10")], id.vars = c("dt","Date","Time","hr"), variable.name = "T")
+t1997.l$Year<- 1997
 
+t1999.l <-  temp.1999[,c("dt","DATE","TIME","hr","Variable", "Value","YEAR")]
+names(t1999.l) <- c("dt","Date","Time","hr","T", "value","Year")
 
-#plot time series
+t2024.l <- melt(temp.2024[,c("dt","Date","Time", "hr", "Logger1.T1","Logger1.T2","Logger1.T3","Logger1.T4","Logger2.T1","Logger2.T2","Logger2.T3","Logger2.T4","Logger3.T1","Logger3.T2","Logger3.T3","Logger3.T4.shadedT")], id.vars = c("dt","Date","Time","hr"), variable.name = "T")
+#drop NAs
+t2024.l<- t2024.l[which(!is.na(t2024.l$value)),]
+t2024.l$Year<- 2024
 
-#-----------
-# calculate daily means for met data from 1999
-dailyhistoriclogger_means <- kinghistmetdata %>%
-  group_by(Variable, DATE) %>%
-  summarise(MeanTemperature = mean(TAIR, na.rm = TRUE), .groups = 'drop')
-
-# Plot daily mean historic temperature distributions 
-ggplot(dailyhistoriclogger_means, aes(x = MeanTemperature, color = Variable, group = Variable)) +
-  geom_density(alpha = 0.7) +
-  scale_fill_viridis_d() +
-  labs(
-    title = "1999 Daily Mean Temperature Distributions",
-    x = "Mean Temperature (°C)",
-    y = "Density",
-    color = "Logger"
-  ) +
-  theme_classic(base_size = 20)
-
-
-
-# Manually assign 'Interval' to historic data 
-dailyhistoriclogger_means <- dailyhistoriclogger_means %>%
-  mutate(Interval = "1999")  
-
-# Assign 'Interval' to the 2024 data (already done in your code)
-dailylogger_means <- dailylogger_means %>%
-  mutate(Interval = factor(Interval)) 
-
-# Combine both 2024 and 1999 data
-combined_means <- bind_rows(
-  dailylogger_means %>% mutate(Source = "2024"),
-  dailyhistoriclogger_means %>% mutate(Source = "1999")
-)
-
-# Plotting
-ggplot() +
-  # Plot density lines for 2024 data
-  geom_density(data = filter(combined_means, Source == "2024"),
-               aes(x = MeanTemperature, color = Interval, group = Interval),
-               size = 1) +
-  
-  # Plot density lines for 1999 data
-  geom_density(data = filter(combined_means, Source == "1999"),
-               aes(x = MeanTemperature, color = Interval, group = Interval),
-               size = 1, alpha = 0.7) +
-  
-  # Color scales for the intervals
-  scale_color_viridis_d(option = "D") +
-  
-  labs(
-    title = "1999 vs 2024 Operative Temp Logger Daily Mean Distribution",
-    x = "Mean Temperature (°C)",
-    y = "Density",
-    color = "Date Interval"
-  ) +
-  theme_classic(base_size = 18) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
+#combine
+tdat<- rbind(t1997.l, t1999.l, t2024.l)
 
 #----------------
-  # Figure 2. TPC comparison
-  
-  #save figure
+#Plot time series
+ggplot(tdat, aes(x=dt,y=value, color=T)) + 
+  geom_line()+facet_grid(Year~.) + guides(color="none")
+
+#drop 2024 air temperature
+tdat<- tdat[-which(tdat$T=="Logger3.T4.shadedT"),]
+
+#average across sensors
+tdat.mean <- tdat %>%
+  group_by(dt, Date, Time, Year) %>%
+  summarise(Tmean = mean(value, na.rm = TRUE), n=length(value))
+
+#-----
+#plot distributions for overlapping data, #1997: 224-238; 1999: 227:237
+Tdist.plot <- ggplot(tdat.mean[which(tdat.mean$dt>223 & tdat.mean$dt<238),], aes(x=Tmean, color=factor(Year), fill=factor(Year), group=factor(Year))) +  geom_density(alpha=0.5)+
+  scale_fill_viridis_d() +scale_color_viridis_d()+
+  theme_classic(base_size = 18)+
+  labs(x = "T mean (°C)", color = "Year", fill="Year") 
+
+#restrict to daylight 223 238 Aug 11-26; Aug 11: 6,8:30; Aug 26: 6:20-8
+tdat.mean$dec.dt<- tdat.mean$dt - floor(tdat.mean$dt)
+tdat.day<- tdat.mean[which(tdat.mean$dec.dt>0.25 & tdat.mean$dec.dt<0.85),]
+tdat.day<- tdat.day[which(tdat.day$dt>223 & tdat.day$dt<238),]
+
+Tdist.day.plot <- ggplot(tdat.day, aes(x=Tmean, color=factor(Year), fill=factor(Year), group=factor(Year))) +  geom_density(alpha=0.5)+
+  scale_fill_viridis_d() +scale_color_viridis_d()+
+  theme_classic(base_size = 18)+ xlim(0,40)+
+  labs(title="doy 223-238, daytime" , x = "T mean (°C)", color = "Year", fill="Year") 
+
+#hourly max, min
+tdat.mean.hr <- tdat %>%
+  group_by(Date, hr, Year) %>%
+  summarise(Tmin = min(value, na.rm = TRUE), Tmax = max(value, na.rm = TRUE), Tmean = mean(value, na.rm = TRUE), n=length(value), .groups = 'drop')
+
+Tdist.hr.plot <- ggplot(tdat.mean.hr, aes(x=Tmax, color=factor(Year), fill=factor(Year), group=factor(Year))) +  geom_density(alpha=0.5)+
+  scale_fill_viridis_d() +scale_color_viridis_d()
+
+#-------------------------
+#temperatures during experiments
+# 1999: 15-25 Aug 1999; doy 227-237
+# 2024: June 22-July 4; July 28-Aug 9; doys 173-185, 209-221
+tdat.mean$doy<- floor(tdat.mean$dt)
+inds1<- which(tdat.mean$Year==1999 & tdat.mean$doy %in% 227:237)
+inds2<- which(tdat.mean$Year==2024 & tdat.mean$doy %in% 173:185)
+inds3<- which(tdat.mean$Year==2024 & tdat.mean$doy %in% 209:221)
+#add study
+tdat.mean$study<- NA
+tdat.mean$study[inds1]<- "Aug 1999"
+tdat.mean$study[inds2]<- "June 2024"
+tdat.mean$study[inds3]<- "July 2024"
+
+Tdist.exp.plot <- ggplot(tdat.mean[which(!is.na(tdat.mean$study)),], aes(x=Tmean, color=factor(study), fill=factor(study), group=factor(study))) +  geom_density(alpha=0.5)+
+  scale_fill_viridis_d() +scale_color_viridis_d()
+
+# just daylight
+tdat.day<- tdat.mean[which(tdat.mean$dec.dt>0.25 & tdat.mean$dec.dt<0.85),]
+
+Tdist.expday.plot <- ggplot(tdat.day[which(!is.na(tdat.day$study)),], aes(x=Tmean, color=factor(study), fill=factor(study), group=factor(study))) +  geom_density(alpha=0.5)+
+  scale_fill_viridis_d() +scale_color_viridis_d()+
+  theme_classic(base_size = 18)+ xlim(0,40)+
+  labs(title="experiments, daytime" , x = "T mean (°C)", color = "Study", fill="Study") 
+
+#----------------
+#Save figures
   if(desktop=="y") setwd("/Users/laurenbuckley/Google Drive/Shared drives/TrEnCh/Projects/WARP/Projects/PrapaeGardenExpt/figures/")
   if(desktop=="n") setwd("/Users/lbuckley/Google Drive/Shared drives/TrEnCh/Projects/WARP/Projects/PrapaeGardenExpt/figures/")
-  pdf("Fig2_PrapaeTPC_2024.pdf",height = 6, width = 15)
-  tpc.plot.h + tpc.plot +tpc.all.plot
+  
+pdf("Fig_Tdist.pdf",height = 6, width = 12)
+Tdist.day.plot + Tdist.expday.plot
   dev.off()
-  
-  
+
   #==================
 
 
+ 
+ 
   
 
-  # Plot all TM columns as separate lines
-  AllTemperaturePlot <- ggplot(histmicroclimdata_long, aes(x = DATETIME, y = Value, color = Variable)) +
-    geom_point() +
-    scale_color_viridis_d()+
-    labs(
-      title = "Historic 1999 Field Seln Microclimate Temperatures",
-      x = "Datetime",
-      y = "Temperature (°C)",
-      color = "Sensor"
-    ) +
-    theme_minimal()
-  
-  #print Temperature plot plot
-  print(AllTemperaturePlot)
-  
-  # Calculate temp extremes for data frame
-  temp_extremes <- histmicroclimdata_long %>%
-    group_by(Variable) %>%
-    summarise(T_min = min(Value, na.rm = TRUE),
-              T_max = max(Value, na.rm = TRUE),
-              .groups = 'drop')
-  
-  # Reshape the temp_extremes data into long format for plotting
-  temp_extremes_long <- temp_extremes %>%
-    pivot_longer(cols = c(T_min, T_max), names_to = "Temperature_Type", values_to = "Temperature")
-  
-  # Min and Max distribution plot for each temperature logger
-  histminmaxplot <- ggplot(temp_extremes_long, aes(x = Variable, y = Temperature, fill = Temperature_Type)) +
-    geom_bar(stat = "identity", position = position_dodge()) +
-    scale_fill_viridis_d() +
-    labs(
-      title = "1999 Field Seln Minimum and Maximum Temperatures by Logger",
-      x = "Logger",
-      y = "Temperature (°C)",
-      fill = "Temperature Type"
-    ) +
-    theme_classic(base_size = 16) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  print(histminmaxplot)
-  
-  # Combine DATE and LONGTIME into a single datetime column
-  histhourly_means <- histmicroclimdata_long %>%
-    mutate(HourlyTime = floor_date(DATETIME, "hour")) %>%
-    group_by(HourlyTime, Variable) %>%
-    summarise(MeanTemperature = mean(Value, na.rm = TRUE),
-              .groups = 'drop')
-  # Calculate hourly means for each temperature logger
-  histhourly_means <- histmicroclimdata_long %>%
-    mutate(HourlyTime = floor_date(DATETIME, "hour")) %>% # Round datetime to the nearest hour
-    group_by(HourlyTime, Variable) %>%                   # Group by rounded hour and logger
-    summarise(MeanTemperature = mean(Value, na.rm = TRUE), .groups = 'drop') # Calculate mean
-  
-  # Plot hourly means for each temperature logger in the field
-  hourly_plot <- ggplot(histhourly_means, aes(x = HourlyTime, y = MeanTemperature, color = Variable)) +
-    geom_line() + # Use line plot to show trends
-    scale_color_viridis_d() + 
-    labs(
-      title = "1999 Field Seln Hourly Mean Temperatures by Logger",
-      x = "Datetime",
-      y = "Mean Temperature (°C)",
-      color = "Sensor"
-    ) +
-    theme_minimal() + # Clean plot style
-    theme(
-      axis.text.x = element_text(angle = 45, hjust = 1)  # Rotate x-axis labels for better readability
-    )
-  
-  # Print the hourly data plot
-  print(hourly_plot)
-  
-  #===========================
-  
-  # Define the start and end dates for filtering
-  start_date <- as.POSIXct("2024-06-22", format = "%Y-%m-%d")
-  end_date <-
-    as.POSIXct("2024-08-15 23:59:59", format = "%Y-%m-%d %H:%M:%S")
-  
-  # Convert datetime to just date for daily means
-  filtered_data_day <- long_data%>%
-    mutate(DATE = as.Date(datetime))
-  
-  # Divide logger information into time intervals so it is easier to visualize, split into experiment times when caterpillars were in the field 
-  filtered_data_day <- long_data %>%
-    mutate(DATE = as.Date(datetime)) %>%
-    mutate(Interval = case_when(
-      DATE >= as.Date("2024-06-21") & DATE <= as.Date("2024-07-03") ~ "2024-06-21 to 2024-07-03",
-      DATE >= as.Date("2024-07-03") & DATE <= as.Date("2024-07-28") ~ "2024-07-03 to 2024-07-28",
-      DATE >= as.Date("2024-07-28") & DATE <= as.Date("2024-08-18") ~ "2024-07-28 to 2024-08-18",
-      TRUE ~ NA_character_
-    )) %>%
-    filter(!is.na(Interval))
-  
-  # Calculate daily mean temperatures for all loggers combined
-  dailylogger_means <- filtered_data_day %>%
-    group_by(DATE, Interval) %>%
-    summarise(MeanTemperature = mean(Temperature, na.rm = TRUE), .groups = 'drop')
-  
-  # Plot daily mean temperature distributions for each time interval indicated above
-  ggplot(dailylogger_means, aes(x = MeanTemperature, color = Interval)) +
-    geom_density(size = 1, alpha = 1) +
-    scale_fill_viridis_d() +
-    labs(
-      title = "2024 Daily Mean Temperature Distributions",
-      x = "Mean Temperature (°C)",
-      y = "Density",
-      fill = "Date Interval"
-    ) +
-    theme_classic(base_size = 18)
-  
-  # Load in historic met data frame
-  kinghistmetdata <- read.csv("FormattedHistoricMetDataFieldSln.csv")
-  
  
